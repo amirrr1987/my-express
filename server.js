56cace2d342ad3a7cd9d7761749b2f592256cb72
Amir Maghami <amaghami@haco.dc>
2022-06-13 12:02:38 +0430

    feat: public
    - create public html template
    - set routes

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-
+const path = require('path');
+app.use((req, res, next) => {
+    console.log(req.url);
+    next();
+})
+app.get('/',(req, res) => {
+    res.sendFile( path.join(__dirname , './public/index.html' ));
+})
+app.get('/about',(req, res) => {
+    res.sendFile( path.join(__dirname , './public/about.html' ));
+})
+app.get('/gallery',(req, res) => {
+    res.sendFile( path.join(__dirname , './public/gallery.html' ));
+})
+app.get('/contact',(req, res) => {
+    res.sendFile( path.join(__dirname , './public/contact.html' ));
+})
 app.listen(port, () => { 'server running on port :' + port });
 
