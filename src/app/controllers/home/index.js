04ba87dc9853b991d5d38be33901b7d78ef585b4
Amir Maghami <amaghami@haco.dc>
2022-07-12 17:18:14 +0430

    feat: validator

--- src/app/controllers/home/index.js
+++ src/app/controllers/home/index.js
@@ -1,6 +1,15 @@
+const Joi = require("joi");
+
 class Controller {
   getIndex(req, res) {
-    res.send({ page: "home" });
+    const schema = Joi.object({
+      name: Joi.string().min(3).max(10).required(),
+    });
+    const { error } = schema.validate(req.query);
+    if (error) {
+      res.send({ error: error.message });
+    }
+    res.send({ query: req.query.name });
   }
 }
 module.exports = new Controller();
